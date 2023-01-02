70053d22c85d161c70b9b8e39e4ff418f0ae362e
mathew180 <mathewadavuruku@gmail.com>
2023-01-02 21:15:24 +0000

    project1

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-const PORT = 3000;
+//reg expression for omitting extension
+app.get("^/$|/index(.html)?", (req, res) => {
+     res.sendFile(path.join(__dirname, "views", "index.html"))
+    // res.send("Hello Mathew")
+    console.log(req.method)
+})
+
+const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Backend is running on port ${PORT}`)
-});
-
-console.log("Hi Node js")
+    console.log(`Backend is currently running on port ${PORT}`)
+})
