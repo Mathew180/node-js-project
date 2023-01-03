cd9fb8590b3c66944921bf0269a739d849be6a21
mathew180 <mathewadavuruku@gmail.com>
2023-01-03 23:18:10 +0000

    project1

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-//reg expression for omitting extension
-app.get("^/$|/index(.html)?", (req, res) => {
-     res.sendFile(path.join(__dirname, "views", "index.html"))
-    // res.send("Hello Mathew")
+app.get("/", (req, res) => {
+    res.send("Hello Mathew")
     console.log(req.method)
 })
 
-const PORT = 3000 || process.env.PORT;
+app.get("^/$|/index(.html)?", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"))
+})
+
+const PORT = 3500 || process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Backend is currently running on port ${PORT}`)
