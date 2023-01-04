98d972efa895893dadde6af7da6b7ef5b7466f32
mathew180 <mathewadavuruku@gmail.com>
2023-01-04 23:15:36 +0000

    project1

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+
+const PORT = 3500 || process.env.PORT;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req, res) => {
     res.send("Hello Mathew")
@@ -11,7 +20,7 @@ app.get("^/$|/index(.html)?", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"))
 })
 
-const PORT = 3500 || process.env.PORT;
+
 
 app.listen(PORT, () => {
     console.log(`Backend is currently running on port ${PORT}`)
